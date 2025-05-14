3d80ffb19b0e694c0c504b47b2d912e1b0191786
Grégoire Leroy <gregoire.leroy86@icloud.com>
2025-05-14 22:20:55 +0200

    Several updates

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -11,7 +11,7 @@ const AboutSection = () => {
       <div className="grid md:grid-cols-2 gap-12 items-center">
         <div>
           <img
-            src="https://images.unsplash.com/photo-1509316975850-ff9c5deb0cd9?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80"
+            src="mousse_foret.jpg"
             alt="Forêt de pins"
             className="rounded-lg shadow-lg w-full h-[500px] object-cover"
           />
